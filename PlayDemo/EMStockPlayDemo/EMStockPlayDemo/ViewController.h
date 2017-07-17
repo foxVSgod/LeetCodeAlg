0e3f5e4f70483c6406a07cb116d1967a664ce081
yourongrong <245252081@qq.com>
2017-07-17 23:40:18 +0800

    重写个股播放器

---
+++ PlayDemo/EMStockPlayDemo/EMStockPlayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EMStockPlayDemo
+//
+//  Created by rongrong you on 2017/7/17.
+//  Copyright © 2017年 rongrong you. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
